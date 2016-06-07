577a382b6de4860c8e620e338b1c00d4ef9e67a3
rockjins <chinajins@gmail.com>
2016-06-07 20:50:27 +0800

    Add files via upload

---
+++ goshow/js/index.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+	var mySwiper = new Swiper('.swiper-container', {
+	autoplay: 3000,
+	 pagination: '.swiper-pagination',
+	 autoplayDisableOnInteraction : false,
+})
+	$.ajax({
+		url:"http://datainfo.duapp.com/shopdata/getGoods.php",
+		type:"get",
+		success:function(data){
+			var index =  data.indexOf("(");
+			console.log(data);
+			 data = data.substring(index+1,data.length-1);
+			 data = JSON.parse(data);
+			 var temp = "";
+			 for(var i = 0;i<data.length;i++){
+			 	temp += '<div class="item">'
+				+'<img src="'+data[i].goodsListImg +'" class="item-img">'
+				+'<div class="item-info">'
+					+'<p class="item-title">'+data[i].goodsName+'</p>'
+					+'<div class="item-left">'
+						+'<p><span>￥'+data[i].price+'</span><span><del>￥secret</del></span></p>'
+						+'<span class="discount">'+data[i].discount+'折</span>'
+					+'</div><div class="item-right"><a class="icon" href="#"><i class="iconfont">&#xe603;</i></a></div></div></div>';
+			 }
+			 $(".item-group").append(temp);
+		}
+	});
+}
