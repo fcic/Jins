6ab5b12c463ad06f885bdc6704c54ccf27878a60
rockjins <chinajins@gmail.com>
2016-06-18 09:58:31 +0800

    1

--- goshow/common/rem.js
+++
@@ -1,4 +0,0 @@
-window.onresize = function(){
-	document.documentElement.style.fontSize = document.documentElement.clientWidth /6.4 + "px";
-}
-window.onresize();
---
+++ test/goshow/common/rem.js
@@ -0,0 +1,4 @@
+window.onresize = function(){
+	document.documentElement.style.fontSize = document.documentElement.clientWidth /6.4 + "px";
+}
+window.onresize();
