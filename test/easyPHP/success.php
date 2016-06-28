59eca284ff64f6341eac21ff6eda59de2e3d4acc
rockjins <chinajins@gmail.com>
2016-06-28 09:19:57 +0800

    php

---
+++ test/easyPHP/success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		$name = $_POST["username"];
+		$pwd = $_POST["pwd"];
+		$fp = fopen("./data.txt", "a");
+		$str = "user:".$name."&password:".$pwd."\n";
+		fwrite($fp,$str);
+		fclose($fp);
+		echo "<h1>欢迎回来,".$name."！<h1>";
+	?>
+</body>
+</html>
